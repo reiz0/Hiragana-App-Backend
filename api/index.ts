cfbfec8a4cdb5c7e9a5f79b01597ba800a858cf9
yuki <yukihiguchi0826@gmail.com>
2024-08-23 19:57:31 -0700

    test for vercel

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { createServer } from "http";
+// import { createServer } from "http";
 import mongoose from "mongoose";
 import { UserRouter } from "./routes/users.routes";
 import "dotenv/config";
@@ -7,7 +7,7 @@ import "dotenv/config";
 const uri = process.env.MONGO_URI;
 
 const app = express();
-const server = createServer(app);
+// const server = createServer(app);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,9 +22,10 @@ if (uri) {
 }
 
 
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
-server.listen(process.env.PORT, () =>
-  console.log(`[Server]: http://${process.env.HOST}:${process.env.PORT}`)
-);
+// server.listen(process.env.PORT, () =>
+//   console.log(`[Server]: http://${process.env.HOST}:${process.env.PORT}`)
+// );
 
 module.exports = app;
