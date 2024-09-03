703bbfaa5c7ab462250a28d58bf4be48f3d83708
yuki <yukihiguchi0826@gmail.com>
2024-09-03 16:04:42 -0700

    make storeNewScore controller

--- api/controllers/quiz.controllers.ts
+++ api/controllers/quiz.controllers.ts
@@ -1,9 +1,42 @@
+import { Request, Response } from "express";
 import { QuizModel } from "../models/quiz.models";
+import { UserModel } from "../models/user.models";
 
-export const getMaxScoreByUser = async (req: Request, res: Response) => {
-  const { levels, user } = req.params;
+export async function getMaxScore(req: Request, res: Response) {
+  const { level, accountName } = req.body;
+  try {
+    const accountScores = await UserModel.findOne({ accountName }).populate({
+      path: "hiraganaScore",
+      match: { level },
+      select: "score",
+    });
 
-  const maxScore = await QuizModel.find({ levels, user });
+    // const scoresArray = []
+    // accountScores.forEach(element => {
 
-  res.json(maxScore);
-};
+    // });
+
+    // const maxScore = accountScores.reduce()
+
+    res.status(201).json(accountScores);
+  } catch {}
+}
+
+export async function storeNewScore(req: Request, res: Response) {
+  const { level, accountName, score } = req.body;
+  try {
+    const existUser = await UserModel.findOne({ accountName });
+    if (!existUser)
+      return res.status(400).send("This Account Name is not exists");
+
+    const newScore = await QuizModel.create({
+      level,
+      score,
+      user: existUser._id,
+    });
+
+    res.status(201).json(newScore);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+}
